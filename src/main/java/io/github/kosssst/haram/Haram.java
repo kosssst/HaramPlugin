70d91b679cc16cc3292ef3469a019e9ef149d95f
Kostia <47629852+kosssst@users.noreply.github.com>
2023-03-20 22:25:09 +0200

    initial commit

---
+++ src/main/java/io/github/kosssst/haram/Haram.java
@@ -0,0 +1,17 @@
+package io.github.kosssst.haram;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Haram extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Haram plugin enabled");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
