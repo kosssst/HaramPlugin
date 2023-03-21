06b685a551d8a14c63008e2a60df7c50415bce56
Kostia <47629852+kosssst@users.noreply.github.com>
2023-03-21 19:29:01 +0200

    added teleportation

--- src/main/java/io/github/kosssst/haram/EventListener.java
+++ src/main/java/io/github/kosssst/haram/EventListener.java
@@ -2,7 +2,10 @@ package io.github.kosssst.haram;
 
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
@@ -14,7 +17,10 @@ public class EventListener implements Listener {
     public void onPlayerEating(PlayerItemConsumeEvent event) {
         ItemStack food = event.getItem();
         Material mat = food.getType();
+        Player player = event.getPlayer();
+        World nether = Bukkit.getWorld("world_nether");
         if ((mat == Material.COOKED_PORKCHOP) || (mat == Material.PORKCHOP)){
+            player.teleport(new Location(nether, 80.0, 42.0, -350.0));
             Bukkit.broadcastMessage("Haram!");
         }
     }
